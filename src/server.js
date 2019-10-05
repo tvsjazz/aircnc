9f3a962f9863ea5b7fd7af6654f5cda8ad15607d
Tiago Siqueira <tvsjazz@gmail.com>
2019-10-05 17:01:52 -0300

    backend pronto

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://tvsjazz:tiago@cluster0-rcmvp.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
